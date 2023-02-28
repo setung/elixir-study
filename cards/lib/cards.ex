799920f8bcdf124dcce45be7cf58d62be83db316
sehun.jang <sehun.jang@illuminarean.com>
2023-02-28 23:05:31 +0900

    test

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -3,11 +3,19 @@
 # Enum 컬랙션 라이브러리
 # 자동 정렬: shift option f
 # mix deps.get 의존성 추가
+# mix docs 문서화
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
+
   def hello do
     "Hello World!"
   end
 
+  @doc """
+    Returns a list of strings representing a deck of playing cards
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -21,6 +29,14 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Determines whether a deck contains a given card
+
+  ## Example
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
@@ -29,6 +45,17 @@ defmodule Cards do
   # color1 = ["red"]
   # [color1] = ["red"]
   # [color1, color2] = ["red", "blue"]
+
+  @doc """
+    Divides a deck into a hand and the remainder of deck.
+    The `hand_size` argument indicates how many cards should be in the hand.
+
+    ## Examples
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
@@ -42,17 +69,18 @@ defmodule Cards do
   def load(filename) do
     case File.read(filename) do
       {:ok, binary} -> :erlang.binary_to_term(binary)
-      {:error, _reason} -> "That file does not exist"   #_reason, 변수가 있다는 것을 알지만 사용하지 않겠다라는 의미.
+      # _reason, 변수가 있다는 것을 알지만 사용하지 않겠다라는 의미.
+      {:error, _reason} -> "That file does not exist"
     end
   end
 
   def create_hand(hand_size) do
-   # deck = Cards.create_deck()
-   # deck = Cards.shuffle(deck)
-   # hand = Cards.deal(deck, hand_size)
+    # deck = Cards.create_deck()
+    # deck = Cards.shuffle(deck)
+    # hand = Cards.deal(deck, hand_size)
 
-    Cards.create_deck
-    |> Cards.shuffle
+    Cards.create_deck()
+    |> Cards.shuffle()
     |> Cards.deal(hand_size)
   end
 end
--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -1,8 +1,22 @@
+# text 실행: mix test
+# doctest -> 문서의 example도 테스트 진행
+
 defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
   test "greets the world" do
-    assert Cards.hello() == :world
+    assert Cards.hello() == "Hello World!"
+  end
+
+  test "create_deck makes 20 cards" do
+    deck_length = length(Cards.create_deck)
+    assert deck_length == 20
+  end
+
+  test "shuffling a deck randmized it" do
+    deck = Cards.create_deck
+    assert deck != Cards.shuffle(deck)
+    refute deck == Cards.shuffle(deck)
   end
 end

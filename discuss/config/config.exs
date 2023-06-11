8e38205212db99ddb14150a41ce66a3f40cc4a81
sehun.jang <sehun.jang@illuminarean.com>
2023-06-11 17:26:07 +0900

    sec12

--- discuss/config/config.exs
+++ discuss/config/config.exs
@@ -65,8 +65,8 @@ config :ueberauth, Ueberauth,
   ]
 
   config :ueberauth, Ueberauth.Strategy.Github.OAuth,
-  client_id: "23a90981b4152252cbf7",
-  client_secret: "34922828c46014b0018953b54fb557ff829c0465"
+  client_id: System.get_env("GITHUB_CLIENT_ID"),
+  client_secret: System.get_env("GITHUB_CLIENT_SECRET")
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
